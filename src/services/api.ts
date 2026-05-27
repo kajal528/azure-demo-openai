b0b0f154d0e189e16592dc62c6fb16ad9f152026
Kajal Dwivedi <kajaldwivedi528@gmail.com>
2026-05-28 00:52:53 +0530

    hardcoded backend url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
+console.log('""""""""""""""""""""""""""""""""""""""""""""""""');
+console.log(import.meta.env.VITE_API_URL);
+console.log('""""""""""""""""""""""""""""""""""""""""""""""""');
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://azure-demo-openai-gwgbf3dyetd7bhge.centralindia-01.azurewebsites.net/",
 });
 
 export default api;
